b5106579184c6d2ba42f635db59c5aaa0bca2a62
Satyajit-dandi <84236694+Satyajit-dandi@users.noreply.github.com>
2023-01-29 18:09:05 +0530

    deployment changes2

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar(props) {
         className={`navbar navbar-expand-lg  navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="#">
             {props.title}
           </a>
           <button
@@ -26,19 +26,19 @@ export default function NavBar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <a className="nav-link" href="#">
                   About
                 </a>
               </li>
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dr opdown-toggle"
-                  href="/"
+                  href="#"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
@@ -47,12 +47,12 @@ export default function NavBar(props) {
                 </a>
                 <ul className="dropdown-menu">
                   <li>
-                    <a className="dropdown-item" href="/">
+                    <a className="dropdown-item" href="#">
                       Action
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="/">
+                    <a className="dropdown-item" href="#">
                       Another action
                     </a>
                   </li>
@@ -60,7 +60,7 @@ export default function NavBar(props) {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a className="dropdown-item" href="/">
+                    <a className="dropdown-item" href="#">
                       Something else here
                     </a>
                   </li>
