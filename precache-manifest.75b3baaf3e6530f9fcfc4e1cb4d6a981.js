506a57808b32b70700bd0abd33f7cb865a56f819
Satyajit-dandi <84236694+Satyajit-dandi@users.noreply.github.com>
2023-01-29 18:59:48 +0530

    Updates

---
+++ precache-manifest.75b3baaf3e6530f9fcfc4e1cb4d6a981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a1b98922aca41bb31b5",
+    "url": "/Text-Utils-React-App/static/css/main.a044d693.chunk.css"
+  },
+  {
+    "revision": "1a1b98922aca41bb31b5",
+    "url": "/Text-Utils-React-App/static/js/main.1a1b9892.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/Text-Utils-React-App/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/Text-Utils-React-App/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "fc1328feacc839ceb135",
+    "url": "/Text-Utils-React-App/static/js/runtime~main.fc1328fe.js"
+  },
+  {
+    "revision": "91113711c0f2cfd3d97f514c861da692",
+    "url": "/Text-Utils-React-App/index.html"
+  }
+];
